6dc1fe086043034e807254e71f684c1970b03350
sanctumQUIRE <a.clohare@gmail.com>
2022-06-14 12:40:04 -0400

    Removal unnecessary line testing

--- src/SuperPower.java
+++ src/SuperPower.java
@@ -4,7 +4,7 @@ public class SuperPower {
 
 	public static void main(String[] args) {
 		String power = JOptionPane.showInputDialog("WHAT IS YOUR SUPER POWER?");
-		// TODO Auto-generated method stub
+		
 		power = power.toUpperCase();
 		JOptionPane.showMessageDialog(null,power+" TO THE RESCUE!");
 	}
